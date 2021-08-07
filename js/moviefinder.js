dee071b2b18c32ca872f040c1dee252cc8a8e69b
Aayush Prasad <45399541+aayuprasad@users.noreply.github.com>
2021-08-08 04:39:03 +0530

    Add files via upload

--- js/moviefinder.js
+++ js/moviefinder.js
@@ -1,11 +1,16 @@
+
+function setItemInSession(){
+    sessionStorage.setItem("movietitle",document.getElementById("movietitle").value);
+    location.href = "movies.html";
+}
+function show_movie_details(id){
+    console.log(id);
+}
 function showDetails(){
-    document.getElementById("details").style.color="white";
-    document.getElementById("details").style.fontSize="20px";
-    // movietitle=document.getElementById("movietitle").value;
     movietitle = sessionStorage.getItem("movietitle")
     var search_header_div = document.getElementById("search_header");
     search_header_div.innerHTML += "<br/>"+"Search results for \""+movietitle+"\":";
-    // document.getElementById("details").innerHTML=movietitle;
+    movies = [];
     httpRequest=new XMLHttpRequest();
     API_KEY="e0ff26e4";
     url="http://www.omdbapi.com/?apikey="+API_KEY+"&s="+movietitle;
@@ -14,6 +19,7 @@ function showDetails(){
     httpRequest.onreadystatechange=function(){
         if(this.readyState==4&&this.status==200){
             movie_data=JSON.parse(this.responseText);
+            idx = 0;
             for (var i = 0; i < movie_data.Search.length; i++) {
                 movie_title=movie_data.Search[i].Title;
                 imdbID=movie_data.Search[i].imdbID;
@@ -25,9 +31,7 @@ function showDetails(){
                 httpRequest2.onreadystatechange=function(){
                     if(this.readyState==4&&this.status==200){
                         var details_div = document.getElementById("details");
-                        // details_div.style.color="blue";
                         mv_data=JSON.parse(this.responseText);
-                        // details_div.innerHTML += "<p>"+this.id+"<p>";
                         id=this.id;
                         poster=mv_data.Poster;
                         title=mv_data.Title;
@@ -36,18 +40,23 @@ function showDetails(){
                         runtime=mv_data.Runtime;
                         genre=mv_data.Genre;
                         plot=mv_data.Plot;
-                        // details_div.innerHTML += "<div class=\"moviecard\" id=\""+id+"\"><div class=\"poster\" id=\"poster"+id+"\"></div><div class=\"moviename\" id=\"moviename"+id+"\">"+title+"</div></div>";
-                        
-
-                        details_div.innerHTML += "<img id=\"poster"+id+"\" src=\""+poster+"\" class=\"poster\" alt = \""+title+"\" title = \""+title+"\"><span class=\"moviename\">&nbsp; "+title+"</span><br/><br/>";
+                        var movie_dict = {
+                            'id': id,
+                            'poster': poster,
+                            'title': title,
+                            'year': year,
+                            'writer': writer,
+                            'runtime': runtime,
+                            'genre': genre,
+                            'plot': plot
+                        };
+                        movies[idx] = movie_dict;
+                        idx += 1;
+                        row_string='<a class=\"movie\" id=\"a'+id+'\" onclick=\"show_movie_details(this.id)\"><div class = \"card\"><img class = \"movie-image\" src = \"' + poster + '\" alt = \"' + title + '\" style=\"width:100%\" onerror=\"this.onerror=null;this.src=\'images/notavailable.jpg\';\"><div class = \"container\"><h4><b>' + title + '</b></h4><p> Writer: ' + writer + '</p></div></div></a>';
+                        details_div.innerHTML += row_string;
                     }
                 }
             }
         }
     }
-}
-
-function setItemInSession(){
-    sessionStorage.setItem("movietitle",document.getElementById("movietitle").value);
-    location.href = "movies.html";
 }
